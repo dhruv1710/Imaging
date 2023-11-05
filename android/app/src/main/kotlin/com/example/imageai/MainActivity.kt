d60ed9267d213c1939421484cf18e84a30527d72
Dhruv Tiwari <dhruv.d.tiwari@gmail.com>
2023-11-05 10:29:07 +0530

    firest commit

---
+++ android/app/src/main/kotlin/com/example/imageai/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imageai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+abstract class Constants {
+  static const String stabilityKey = String.fromEnvironment(
+    'STABILITY_KEY',
+    defaultValue: '',
+  );
+}
